c81d068c254761e89e9ba41bdc5ef9dbd5b4352f
JuliusMakari <muragejmakari@gmail.com>
2024-01-17 15:09:31 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const input = document.getElementById('new-task-description').value;
+    handleTodos(input);
+    
+    form.reset();
+  });
+
+  function handleTodos(description) {
+    const newTask = document.createElement('li');
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.addEventListener("click", handleDelete);
+    deleteButton.textContent = "X";
+    
+    newTask.textContent = description;
+    newTask.appendChild(deleteButton);
+    
+    document.querySelector('#tasks').appendChild(newTask);
+    console.log(description);
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
 });
